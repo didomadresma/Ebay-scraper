c9cf7eef9c17151615d93c7abb5110c4f0d61d0e
gzhovnu <g.zhovnu@gmail.com>
2016-05-06 18:25:35 +0300

    price is fixed

--- main.py
+++ main.py
@@ -5,9 +5,8 @@ import requests
 import re
 
 
-def parse_ebay(page_num=1):
-    r = requests.get(
-        'http://www.ebay.com/sch/iPads-Tablets-eBook-Readers/171485/i.html?LH_BIN=1&_dcat=171485&rt=nc&LH_ItemCondition=1000')
+def parse_ebay(ebay_link, page_num=1):
+    r = requests.get(ebay_link)
     price_title = {}
     next_page_link = ''
     for _ in range(page_num):
@@ -21,10 +20,14 @@ def parse_ebay(page_num=1):
             link = elem.find('h3', attrs={'class': 'lvtitle'}).find('a').get('href')
             # print link
             price = re.findall("\d+\.\d+", price)
+            price_link = []
             if len(price) != 1:
-                price_title[title] = price[1]+", "+link
+                price_link.append(price[1])
+                price_link.append(link)
             else:
-                price_title[title] = price[0]+", "+link
+                price_link.append(price[0])
+                price_link.append(link)
+            price_title[title] = price_link
         next_page_link = soup.find('a', attrs={'class': 'gspr next'}).get('href')
     return price_title
 
@@ -44,19 +47,19 @@ def parse_amazon(titles_prices):
                 a_link = soup.find('a', attrs={'class': 'a-link-normal s-access-detail-page  a-text-normal'}).get('href')
                 a_price = soup.find('span', attrs={'class': 'a-size-base a-color-price s-price a-text-bold'}).text
                 a_price = re.findall("\d+\.\d+", a_price)
-                # print ("Ebay price: '%s' ----------------> Amazon price: '%s'" % (e_price, a_price[0]))
+                print a_price[0]
+                print e_price[0]
                 if float(a_price[0]) > float(e_price[0]):
                     with open('./output/results.txt', 'a') as outfile:
-                        # json.dump(price_title, outfile)
                         outfile.write("Ebay price: {}, Amazon price: {}, product name: {}, {}\n\n".format(str(e_price), str(a_price[0]), str(title), a_link))
-                    print a_price
+                    # print a_price
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Run parser')
-    # parser.add_argument("--ebay_link", dest="ebay_link", required=True,
-    #                     help="Ebay link to parse", metavar="")
+    parser.add_argument("--ebay_link", dest="ebay_link", required=True,
+                        help="Summary page of Ebay with buy it now and new lots ", metavar="")
     parser.add_argument("--page_num", dest="page_num", required=True,
                         help="Page numbers to parse", metavar="")
     args = parser.parse_args()
-    ebay_results = parse_ebay(page_num=int(args.page_num))
+    ebay_results = parse_ebay(args.ebay_link, page_num=int(args.page_num))
     parse_amazon(ebay_results)
